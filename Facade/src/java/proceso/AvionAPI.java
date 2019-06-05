cbc6947d5b03729aef58ccf51babd5f618574d7c
RayanSt <49204068+RayanSt@users.noreply.github.com>
2019-06-05 00:14:14 -0500

    Update AvionAPI.java

--- Facade/src/java/proceso/AvionAPI.java
+++ Facade/src/java/proceso/AvionAPI.java
@@ -8,10 +8,7 @@ public class AvionAPI {
 		String todo = "";
                 todo = "</Br>"+"=============================="+"</Br>"+"Vuelos encontrados para " + destino + " desde " + origen+
                         "</Br>"+"Fecha IDA: " + fechaIda + " Fecha Vuelta " + fechaVuelta+"</Br>"+"=============================="+"</Br>";
-//                System.out.println("==============================");
-//		System.out.println("Vuelos encontrados para " + destino + " desde " + origen);
-//		System.out.println("Fecha IDA: " + fechaIda + " Fecha Vuelta " + fechaVuelta);
-//		System.out.println("==============================");
+
                 return todo;
 	}
 
